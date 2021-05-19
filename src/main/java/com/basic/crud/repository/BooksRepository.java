293ed76f1d5da1eb5da50de821a2af238bc7b9db
shallon kobusinge <kobshallon041@gmail.com>
2021-05-19 10:59:00 +0200

    Fix: Changed Passowrd

--- src/main/java/com/basic/crud/repository/BooksRepository.java
+++ src/main/java/com/basic/crud/repository/BooksRepository.java
@@ -2,8 +2,11 @@ package com.basic.crud.repository;
 
 import com.basic.crud.model.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Books,Integer> {
+
+//    @Query(value = "Select author from books author where ")
 }
